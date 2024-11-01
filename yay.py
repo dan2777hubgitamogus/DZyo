3156cdac2deca7d9b5453fd8d9113f22d619a870
admir <victorijadzyoba@gmail.com>
2024-11-01 19:19:42 +0200

    Add file

--- yay.py
+++ yay.py
@@ -3,8 +3,10 @@ class Product:
         self.price = 190
         self.name = "product"
         self.quantity = 30
+        self.calculate_total_price = self.price * self.quantity
+        self.display_info = "The computer game"
     def printer(self):
-        print(f"The {self.name} is cost {self.price}, there are {self.quantity} items")
+        print(f"The {self.name} is cost {self.price}, there are {self.quantity} items,total price is{self.calculate_total_price},{self.display_info}")
 
 product1=()
 product1.printer()
